db3bd20b87f33db8d02f7961794c99d95bc6b0e0
Gerson Zuñiga <zunigagerson@hotmail.com>
2024-04-28 17:08:58 -0500

    [Feat]: add retrieve evaluations function (#17)

--- src/db/evaluation.ts
+++ src/db/evaluation.ts
@@ -1,4 +1,6 @@
+import type { User } from "@supabase/supabase-js";
 import { supabase } from "./client";
+import { retrieveUser } from "./users/auth";
 
 export const insertDefaultEvaluations = async (id_child: string) => {
   try {
@@ -9,6 +11,46 @@ export const insertDefaultEvaluations = async (id_child: string) => {
         { id_child, id_practice: "e1bc9382-de38-43c4-bd53-726f0e01967d" },
       ])
       .select();
+
+    if (error) {
+      return { data, error: true, message: error.message };
+    }
+
+    return { data, error: false };
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const retrieveEvaluations = async () => {
+  try {
+    const userInfo = await retrieveUser();
+
+    if (userInfo === undefined || userInfo.error === true) {
+      return { data: userInfo, error: true };
+    }
+
+    const userData = userInfo.data as User;
+    const idUser = userData.id;
+
+    // prettier-ignore
+    let { data, error } = await supabase
+    .from('PROFILE')
+    .select(`
+      id_user, first_name, last_name,
+      PARENT(
+        CHILD(
+          first_name, last_name,
+          EVALUATION(
+            id_evaluation, finished_at, evaluated_at,
+            PRACTICE(
+              id_practice, title, description, type
+            )
+          )
+        )
+      )
+    `)
+    .eq('id_user', idUser);
 
     if (error) {
       return { data, error: true, message: error.message };
